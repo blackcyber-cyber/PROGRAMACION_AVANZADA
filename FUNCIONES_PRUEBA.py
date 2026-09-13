c581b7f35cd419c39a457eaf738f82f561826870
Hugo <hugocristian778@gmail.com>
2026-09-12 22:33:09 -0600

    agrega funcion suma y prueba de importacion

---
+++ FUNCIONES_PRUEBA.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 22:21:28 2026
+
+@author: hugoc
+"""
+
+def suma(a, b):
+    return a - b + a**2
---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 22:22:37 2026
+
+@author: hugoc
+"""
+
+import FUNCIONES_PRUEBA as fp
+
+print(fp.suma(2, 3))
