11cf649a06699a24cbca577d4fe11a2cfb443391
Hugo <hugocristian778@gmail.com>
2026-09-16 14:33:50 -0600

    Se soluciono el problema de merge en la rama de cataologo

--- funciones_complementarios/catalogo.py
+++ funciones_complementarios/catalogo.py
@@ -3,21 +3,10 @@
 Módulo de catálogo de productos.
 """
 
-
 def cargar_catalogo():
     """
     """
     catalogo = {
-<<<<<<< Updated upstream
-        "P001": {"nombre": "Café", "precio": 20.0, "stock": 20},
-        "P002": {"nombre": "Té", "precio": 25.0, "stock": 20},
-        "P003": {"nombre": "Galletas", "precio": 24.0, "stock": 40},
-        "P004": {"nombre": "Chocolate", "precio": 50.5, "stock": 10},
-        "P005": {"nombre": "Agua embotellada", "precio": 16.0, "stock": 60},
-        "P006": {"nombre": "Refresco 2L", "precio": 29.5, "stock": 40},
-        "P007": {"nombre": "Sabritas", "precio": 20.0, "stock": 70},
-        "P008": {"nombre": "Dulces", "precio": 3.0, "stock": 80},
-=======
         "P001": {"nombre": "🍵Café", "precio": 20.0, "stock": 20},
         "P002": {"nombre": "🫖Té", "precio": 25.0, "stock": 20},
         "P003": {"nombre": "🍪Galletas", "precio": 24.0, "stock": 40},
@@ -25,8 +14,7 @@ def cargar_catalogo():
         "P005": {"nombre": "🫗Agua embotellada", "precio": 16.0, "stock": 60},
         "P006": {"nombre": "🥤Refresco 2L", "precio": 29.5, "stock": 40},
         "P007": {"nombre": "🍟Sabritas", "precio": 20.0, "stock": 70},
-        "P008": {"nombre": "🍬Dulces", "precio": 3.0, "stock": 80},
->>>>>>> Stashed changes
+        "P008": {"nombre": "🍬Dulces", "precio": 3.0, "stock": 80}
     }
     return catalogo
 
