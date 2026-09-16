e45a0866c96973ddff6a30d5a4ef7952c4462416
Hugo <hugocristian778@gmail.com>
2026-09-16 15:25:17 -0600

    Se modifico el apartado de catalogo, y se corrigio los problemas de repetcion

--- funciones_complementarios/catalogo.py
+++ funciones_complementarios/catalogo.py
@@ -4,8 +4,7 @@ Módulo de catálogo de productos.
 """
 
 def cargar_catalogo():
-    """
-    """
+ 
     catalogo = {
         "P001": {"nombre": "🍵Café", "precio": 20.0, "stock": 20},
         "P002": {"nombre": "🫖Té", "precio": 25.0, "stock": 20},
@@ -14,9 +13,6 @@ def cargar_catalogo():
         "P005": {"nombre": "🫗Agua embotellada", "precio": 16.0, "stock": 60},
         "P006": {"nombre": "🥤Refresco 2L", "precio": 29.5, "stock": 40},
         "P007": {"nombre": "🍟Sabritas", "precio": 20.0, "stock": 70},
-<<<<<<< Updated upstream
-        "P008": {"nombre": "🍬Dulces", "precio": 3.0, "stock": 80}
-=======
         "P008": {"nombre": "🍬Dulces", "precio": 3.0, "stock": 80},
     }
     return catalogo
